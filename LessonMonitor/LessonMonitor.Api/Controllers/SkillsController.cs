16a16647daf5b2d3f6ba235dd1c5d02b4fa164ea
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2021-12-23 15:16:21 +0300

    homework/1 add controller

---
+++ LessonMonitor/LessonMonitor.Api/Controllers/SkillsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace LessonMonitor.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SkillsController : ControllerBase
+    {
+        private static readonly string[] secondScills = new[]
+        {
+            "Music", "Painting", "Sport", "TvShow", "Modelling", "Fishing"
+        };
+
+        private static readonly string[] Name = new[]
+        {
+            "Sergey", "Maria", "Ivan", "Mihail", "Svetlana", "Anton", "Irina"
+        };
+
+        public SkillsController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rnd = new Random();
+            var skills = new List<Skills>();
+
+            for (int i = 0; i < Name.Length; i++)
+            {
+                var skill = new Skills
+                {
+                    Name = Name[rnd.Next(Name.Length)],
+                    Grid = rnd.Next(1, 5),
+                    SecondSkills = secondScills[rnd.Next(secondScills.Length)]
+                };
+                skills.Add(skill);
+            }
+            return Ok(skills);
+        }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.Api/Skills.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.Api
+{
+    public class Skills
+    {
+        public string Name { get; set; }
+        public int Grid { get; set; }
+        public string SecondSkills { get; set; }
+    }
+}
